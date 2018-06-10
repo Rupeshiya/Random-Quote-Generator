225b242bdc66d981e174bee518f51cc1cdcb31d8
Rupesh Krishna Jha <rupeshiya@gmail.com>
2018-06-11 00:23:43 +0530

    initial test

---
+++ Random quote/test.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  var quote;
+  var author;
+  function newQuote() {
+    $.ajax({
+      url: "https://api.forismatic.com/api/1.0/",
+      jsonp: "jsonp",
+      dataType: "jsonp",
+      data: {
+        method: "getQuote",
+        lang: "en",
+        format: "jsonp"
+      },
+      success: function(apiResponse) {
+        author = apiResponse.quoteAuthor;
+        quote = apiResponse.quoteText;
+        if (author) {
+          author = "said by " + author;
+          $('#author').text(author);
+        } else {
+          $('#author').text("Unknown");
+        }
+        $('.quote').text(quote);
+
+      }
+    });
+  };
+  newQuote();
+
+  $('#get-quote').click(function(event) {
+    event.preventDefault();
+    newQuote();
+  });
+
+  $('#share-quote').click(function() {
+    window.open("https://twitter.com/intent/tweet?text=" + encodeURIComponent(quote + " — " + author));
+  });
+});
